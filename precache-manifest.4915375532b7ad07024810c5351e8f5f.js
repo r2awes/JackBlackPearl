c903af042df2e905c8830f59eac7be7fe360eb8f
r2awes <r2awes@gmail.com>
2018-12-17 14:11:02 -0500

    Updates

---
+++ precache-manifest.4915375532b7ad07024810c5351e8f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5743890c8e476f10b8e07a86559250c3",
+    "url": "/JackBlackPearl/static/media/BlackPearl.5743890c.ttf"
+  },
+  {
+    "revision": "c17652698adde6835d9c",
+    "url": "/JackBlackPearl/static/js/runtime~main.c1765269.js"
+  },
+  {
+    "revision": "5db56425511559a2211f",
+    "url": "/JackBlackPearl/static/js/main.5db56425.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/JackBlackPearl/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "5db56425511559a2211f",
+    "url": "/JackBlackPearl/static/css/main.9ca956f6.chunk.css"
+  },
+  {
+    "revision": "0b8654629af5f01122ad47e0ffcb52f4",
+    "url": "/JackBlackPearl/index.html"
+  }
+];
