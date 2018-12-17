ec08fb06ab3cb54a19816375d2f78f72d8ddf100
r2awes <r2awes@gmail.com>
2018-12-17 14:16:02 -0500

    Updates

---
+++ precache-manifest.b4de48ff1bb82d3e82b1874ad1f0f27c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "353e204703aa387b63d6ccb23f27edf5",
+    "url": "/JackBlackPearl/static/media/richard.353e2047.jpg"
+  },
+  {
+    "revision": "6217f4289c18942ff2ff",
+    "url": "/JackBlackPearl/static/css/main.ec06c0b8.chunk.css"
+  },
+  {
+    "revision": "0f2181b9654739fa0a06",
+    "url": "/JackBlackPearl/static/js/1.0f2181b9.chunk.js"
+  },
+  {
+    "revision": "c17652698adde6835d9c",
+    "url": "/JackBlackPearl/static/js/runtime~main.c1765269.js"
+  },
+  {
+    "revision": "8cd54068a3439b6b6c85b59f03a3852f",
+    "url": "/JackBlackPearl/static/media/logo.8cd54068.svg"
+  },
+  {
+    "revision": "fe70ddd1afdf98d4d3a93bd52bc15057",
+    "url": "/JackBlackPearl/static/media/wave.fe70ddd1.svg"
+  },
+  {
+    "revision": "6217f4289c18942ff2ff",
+    "url": "/JackBlackPearl/static/js/main.6217f428.chunk.js"
+  },
+  {
+    "revision": "f0eafb24f6aa3aae174c39c45c1e9c4e",
+    "url": "/JackBlackPearl/static/media/sean.f0eafb24.jpg"
+  },
+  {
+    "revision": "8e40b73b22c21bdad768a45dfa1ad926",
+    "url": "/JackBlackPearl/static/media/martin.8e40b73b.jpg"
+  },
+  {
+    "revision": "125ece83e4fe132a0c35b0fe7a2ac4b3",
+    "url": "/JackBlackPearl/static/media/lucy.125ece83.jpg"
+  },
+  {
+    "revision": "cc187346f49abd6f273fd9229cbb253c",
+    "url": "/JackBlackPearl/static/media/alex.cc187346.jpg"
+  },
+  {
+    "revision": "5743890c8e476f10b8e07a86559250c3",
+    "url": "/JackBlackPearl/static/media/BlackPearl.5743890c.ttf"
+  },
+  {
+    "revision": "5218facd89cdeebf0d4d705c89b0ecf2",
+    "url": "/JackBlackPearl/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JackBlackPearl/precache-manifest.765fe8288b1b2e28c793546c61524d7c.js"
+  "/JackBlackPearl/precache-manifest.b4de48ff1bb82d3e82b1874ad1f0f27c.js"
 );
 
 workbox.clientsClaim();
